969b5c9b14dd36d06d8cfb5fecd043e010cff4d2
andyandreone <andy1.-@hotmail.com>
2021-08-11 01:14:32 -0300

    servidor con express

---
+++ index.js
@@ -0,0 +1,53 @@
+const { count } = require('console');
+const { O_NONBLOCK } = require('constants');
+const express = require('express');
+const fs = require('fs')
+
+const app = express();
+app.listen(8080, ()=>{
+    console.log('Escuchando en el puerto 8080');
+})
+
+let countItem=0;
+let countItems=0;
+
+//PUNTO 1
+app.get('/items',(req,res)=>{
+    const obj = {
+        items : [],
+        cantidad: 0
+    }
+    countItems++;
+    fs.promises.readFile('./productos.txt').then(data => data.toString('utf-8')).then(datos =>{
+        obj.items = JSON.parse(datos);
+        obj.cantidad = obj.items.length;
+        res.json(obj)
+    })
+})
+
+//PUNTO 2
+app.get('/item-random', (req,res)=>{
+    let rand = (min,max)=>{
+     return Math.floor(Math.random()*(max-min))+min;
+    }
+    countItem++;
+    fs.promises.readFile('./productos.txt').then(data => data.toString('utf-8')).then(datos =>{
+        const json = JSON.parse(datos)
+            res.json({item:json[rand(0,json.length-1)]})
+    })
+    
+})
+
+//PUNTO 3
+app.get('/visitas',(req,res)=>{
+    let obj = {
+        visitas:{
+            item : countItem,
+            items: countItems
+        }
+    }
+
+    res.json(obj)
+
+})
+
